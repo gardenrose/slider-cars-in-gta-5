7580d64c90a63470e6e534f27def8065b093d545
gardenrose <44342235+gardenrose@users.noreply.github.com>
2022-01-24 12:27:19 +0100

    Add files via upload

---
+++ auti/main.js
@@ -0,0 +1,35 @@
+function show1(){
+    document.getElementById('c2').style.visibility="hidden";
+    document.getElementById('c3').style.visibility="hidden";
+    document.getElementById('c4').style.visibility="hidden";
+    document.getElementById('c5').style.visibility="hidden";
+    document.getElementById('c1').style.visibility="visible";
+    }
+    function show2(){
+    document.getElementById('c1').style.visibility="hidden";
+    document.getElementById('c3').style.visibility="hidden";
+    document.getElementById('c4').style.visibility="hidden";
+    document.getElementById('c5').style.visibility="hidden";
+    document.getElementById('c2').style.visibility="visible";
+    }
+    function show3(){
+    document.getElementById('c1').style.visibility="hidden";
+    document.getElementById('c2').style.visibility="hidden";
+    document.getElementById('c4').style.visibility="hidden";
+    document.getElementById('c5').style.visibility="hidden";
+    document.getElementById('c3').style.visibility="visible";
+    }
+    function show4(){
+    document.getElementById('c1').style.visibility="hidden";
+    document.getElementById('c3').style.visibility="hidden";
+    document.getElementById('c2').style.visibility="hidden";
+    document.getElementById('c5').style.visibility="hidden";
+    document.getElementById('c4').style.visibility="visible";
+    }
+    function show5(){
+    document.getElementById('c1').style.visibility="hidden";
+    document.getElementById('c3').style.visibility="hidden";
+    document.getElementById('c4').style.visibility="hidden";
+    document.getElementById('c2').style.visibility="hidden";
+    document.getElementById('c5').style.visibility="visible";
+    }
